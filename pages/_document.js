c136e1dec3dcb415941d9b302d371b82e9a2b5d3
Florian Shllaku <florian.shllaku@gmail.com>
2025-10-17 12:18:37 +0200

    Make changes to the packages, and add new price list

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&family=Sora:wght@300;400;500;600;700&family=Roboto:wght@300;400;500;700&display=swap"
           rel="stylesheet"
         />
       </Head>
